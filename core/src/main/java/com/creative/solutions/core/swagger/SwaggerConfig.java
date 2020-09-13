dd1c307621e5f2c77ff4e85b9caa888395fb3356
vishmajalkar <vish1985@gmail.com>
2020-09-13 17:30:50 +0200

    swagger package updated

--- core/src/main/java/com/creative/solutions/core/swagger/SwaggerConfig.java
+++ core/src/main/java/com/creative/solutions/core/swagger/SwaggerConfig.java
@@ -19,7 +19,7 @@ public class SwaggerConfig {
     public Docket api() {
         return new Docket(DocumentationType.SWAGGER_2)
         .select()
-        .apis(RequestHandlerSelectors.basePackage("com.example.client"))
+        .apis(RequestHandlerSelectors.basePackage("com.creative.solutions.core.controller"))
         .paths(PathSelectors.any())
         .build();
     }
